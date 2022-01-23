d0bc006bfd990471edb8ef4981eeaec899044126
Ricardo Presilla <rpresilla@initiumsoft.com>
2022-01-23 12:35:04 -0400

    Added file app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Ejemplo #3: Depurando desde nodejs.");
+let texto = "Ejemplo";
+console.log(texto);
+texto = texto + " #3: ";
+
+debugger;
+var x = 2;
+let cuadrado = x =>{
+    return x*x;
+};
+//
+console.log("El cuadrado es: %d",x);
